e23fec11ce0cdeb4c2f22a666e6c5197c508f344
Nathan <nworth013@gmail.com>
2024-03-13 18:55:11 -0700

    Begun work on test cases
    Started making javascript test cases and created a cart page to redirect to from the details page.

--- code/unit_testing/detailsPageTesting.js
+++ code/unit_testing/detailsPageTesting.js
@@ -1,11 +1,12 @@
-function setUpTestingEnvironment() {
-
-}
-
 function testDisplay() {
-    setUpTestingEnvironment();
-   
 
+    if (document.readyState=='complete'){
+        console.log("Success: My Details page is displayed correctly.");
+    } else {
+        console.error("Error: My Details page is incorrectly displayed");
+    }
+   
+    //  http://127.0.0.1:5500/code/Details.html
 }
 
 testDisplay()
