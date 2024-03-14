314fd4f0a8013d595eb45586f037f59ebebdccb2
EricSmith10 <10smith.eric@gmail.com>
2024-03-13 18:17:40 -0700

    added script python file

---
+++ Database/mysite/myapp/addEntryScript.py
@@ -0,0 +1,11 @@
+import datetime
+#from .views import addConcert 
+from .views import addArtist
+
+#TODO add all entries for artists and concerts
+
+
+addArtist(1, "The Electric Tigers", "https://drive.google.com/file/d/1frS-3GGUeoOrFW6Vpk36Aup_1mCnHud1/view?usp=sharing")
+
+
+#addConcert(ConcertId, ArtistId, ConcertDate, Venue, City, TicketQuantity, TicketPrice):
