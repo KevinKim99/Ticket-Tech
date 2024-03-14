8b40d50b0aa05fe52136c226448594903d7d0328
Nathan <nworth013@gmail.com>
2024-03-13 19:18:39 -0700

    Created two test cases
    I have made a test case for the page loading correcting, and also for the page being on the correct domain, it is currently the domain of the test server I'm using.

--- code/unit_testing/detailsPageTesting.js
+++ code/unit_testing/detailsPageTesting.js
@@ -1,12 +1,21 @@
-function testDisplay() {
+let host = '127.0.0.1:5500'
 
-    if (document.readyState=='complete'){
+function testPage() {
+
+    if (document.readyState=='loading'){
         console.log("Success: My Details page is displayed correctly.");
     } else {
         console.error("Error: My Details page is incorrectly displayed");
     }
-   
-    //  http://127.0.0.1:5500/code/Details.html
+        //  http://127.0.0.1:5500/code/Details.html
+    if (self.location.host == host){
+        console.log("Sucess: The website has the correct domain")
+    } else {
+        console.error("Error: The website is not on the correct domain");
+    }
+
+
+
 }
 
-testDisplay()
+testPage()
