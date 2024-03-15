5ef10e08e1763d3697f14c3dc048132a20ffcb2b
EricSmith10 <10smith.eric@gmail.com>
2024-03-15 09:02:13 -0700

    started testing, not finished

--- Database/mysite/myapp/views.py
+++ Database/mysite/myapp/views.py
@@ -32,4 +32,7 @@ def addArtist(ArtistId, ArtistName, ArtistImage):
     artist = Artists(ArtistId = ArtistId, ArtistName = ArtistName, ArtistImage = ArtistImage)
     artist.save()
 
+
+#TODO Add all artists and concerts
 addArtist(3, "The Funky Monkeys", "https://drive.google.com/file/d/11AxDiz6NpGn4X60yPmuJMe85alfaS-LW/view?usp=sharing")
+
